d4ee576f593412d29a7b2ff1e6d107863b516178
rayyansarkhot <rayyanrsarkhot@gmail.com>
2023-06-21 14:31:04 -0400

    Added test files

--- fileSave/main.py
+++ fileSave/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, UploadFile, File
 
-app = FastAPI()
+app = FastAPI(
+    max_request_size=100 * 1024 * 1024,  # 100 MB
+    max_response_size=100 * 1024 * 1024  # 100 MB
+    )
 
 @app.post("/")
 async def upload_file(file: UploadFile = File(...)):
+    print(file)
     file_path = f"saves\{file.filename}"  # Specify the desired save location
     with open(file_path, "wb") as f:
         f.write(await file.read())
